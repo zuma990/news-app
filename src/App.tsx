2a0aad59b676616fd4c90ba5cf959d20b35efd36
zuma990 <zuma@gmail.com>
2023-03-08 13:08:45 +0100

    Fixed style with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
-import MainRouter from './MainRouter'
+import React from "react";
+import "./App.css";
+import MainRouter from "./MainRouter";
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <MainRouter />
     </div>
   );
--- src/MainRouter.tsx
+++ src/MainRouter.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home'
-import ArticlePage from './components/ArticlePage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import ArticlePage from "./components/ArticlePage";
 import Header from "./components/Header";
 function MainRouter() {
-    return (
-        <div>
-            <Header /><br/>
-            <Routes>
-                <Route path='/' element={<Home />}></Route>
-               <Route path='/:id' element={<ArticlePage />}></Route>
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <br />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/:id" element={<ArticlePage />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default MainRouter;
